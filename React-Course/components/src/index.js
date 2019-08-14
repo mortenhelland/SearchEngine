1317ce2f4ea0c235dc968c7133047a069184bc82
Morten Helland <mortenhelland@outlook.com>
2019-08-14 12:14:09 +1000

    :construction:

--- React-Course/components/src/index.js
+++ React-Course/components/src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
-import faker from 'faker';
-import CommentsDetail from './CommentsDetail';
+import React from "react";
+import ReactDOM from "react-dom";
+import CommentsDetail from "./CommentsDetail";
+import faker from "faker";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
-    return (
-        <div className = "ui container comments">
-            <CommentsDetail author ="Sam"/>
-            <CommentsDetail author = "Alex"/>
-            <CommentsDetail author = "Jane"/>
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure oy want to do this?
         </div>
+      </ApprovalCard>
 
-    );
+      <ApprovalCard>
+        <CommentsDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          content="Really bad!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentsDetail
+          author="Alex"
+          timeAgo="Today at 2:00PM"
+          content="wow!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentsDetail
+          author="Jane"
+          timeAgo="Yesterday at 5:00 PM"
+          content="Nice blog!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+    </div>
+  );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
